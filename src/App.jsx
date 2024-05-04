8e504e0325355c9294ed63eb1baf6774d8738ff2
Balzac <balzac.baudemont@gmail.com>
2024-05-04 10:56:51 +0200

    format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Canvas } from '@react-three/fiber'
-import { Box } from './Box';
+import { Box } from './Box'
 
 export function App() {
-    return (
-      <Canvas camera={{ position: [0, 0, 2] }}>
-        <Box position={[-0.75, 0, 0]} name="A" />
-        <Box position={[0.75, 0, 0]} name="B" />
-      </Canvas>
-    );
-  }
+  return (
+    <Canvas camera={{ position: [0, 0, 2] }}>
+      <Box position={[-0.75, 0, 0]} name="A" />
+      <Box position={[0.75, 0, 0]} name="B" />
+    </Canvas>
+  )
+}
