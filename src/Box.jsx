2ee3ee135e05ad67884807ddd11d98e4fcc6145c
Balzac <balzac.baudemont@gmail.com>
2024-05-04 10:56:57 +0200

    adding animation

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,4 +1,14 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react"
+
 export function Box(props) {
+  const ref = useRef();
+
+  useFrame((_, delta) => {
+    ref.current.rotation.x += 1 * delta;
+    ref.current.rotation.y += 0.5 * delta;
+  });
+  
   return (
     <mesh {...props} ref={ref}>
       <boxGeometry />
