efc8780b1816acea7059e8cffb9558a736608dec
Balzac <balzac.baudemont@gmail.com>
2024-05-04 11:14:01 +0200

    add listener and state

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,16 +1,23 @@
-import { useFrame } from "@react-three/fiber";
-import { useRef } from "react"
+import { useFrame } from '@react-three/fiber'
+import { useRef, useState } from 'react'
 
 export function Box(props) {
-  const ref = useRef();
+  const ref = useRef()
+  const [isRotating, setIsRotating] = useState(false)
 
   useFrame((_, delta) => {
-    ref.current.rotation.x += 1 * delta;
-    ref.current.rotation.y += 0.5 * delta;
-  });
-  
+    if (isRotating) {
+      ref.current.rotation.x += 1 * delta
+      ref.current.rotation.y += 0.5 * delta
+    }
+  })
+
   return (
-    <mesh {...props} ref={ref}>
+    <mesh
+      {...props}
+      ref={ref}
+      onPointerDown={() => setIsRotating(true)}
+      onPointerUp={() => setIsRotating(false)}>
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe />
     </mesh>
