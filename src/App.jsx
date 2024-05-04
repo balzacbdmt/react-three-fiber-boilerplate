b928600626d1c97da905e772b92903213f99718d
Balzac <balzac.baudemont@gmail.com>
2024-05-04 14:23:42 +0200

    adding 3D helpers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Canvas } from '@react-three/fiber'
 import { Box } from './Box'
+import { Stats, OrbitControls } from '@react-three/drei'
 
 export function App() {
   return (
     <Canvas camera={{ position: [0, 0, 2] }}>
       <Box position={[-0.75, 0, 0]} name="A" />
       <Box position={[0.75, 0, 0]} name="B" />
+      <Stats />
+      <OrbitControls />
+      <axesHelper />
+      <gridHelper />
     </Canvas>
   )
 }
