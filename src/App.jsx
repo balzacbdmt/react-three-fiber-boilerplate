693b49bd63d46dad49e26be1f60e37e4067c8352
Balzac <balzac.baudemont@gmail.com>
2024-05-04 19:18:38 +0200

    Discovering Environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { Stats, OrbitControls, Circle, useGLTF, Stage } from '@react-three/drei'
+import {
+  Stats,
+  OrbitControls,
+  Environment,
+  Stage,
+  Sphere
+} from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
 import Truck from './Truck'
@@ -8,21 +14,17 @@ export default function App() {
   const gltf = useLoader(GLTFLoader, '/models/monkey.glb')
 
   return (
-    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
+    <Canvas camera={{ position: [-0.5, 1, 2] }}>
+      <Environment preset="forest" background blur={0.5} />
       <directionalLight
         position={[3.3, 1.0, 4.4]}
         castShadow
-        intensity={Math.PI * 2}
-      />
-      <primitive
-        object={gltf.scene}
-        position={[2, 1, 0]}
-        children-0-castShadow
-      />
+        intensity={Math.PI * 2}>
+        <Sphere args={[0.25]} />
+      </directionalLight>
+      <primitive object={gltf.scene} position={[2, 1, 0]} />
       <Suspense fallback={null}>
-        <Stage preset="rembrandt" intensity={1} environment="city">
-          <Truck />
-        </Stage>
+        <Truck />
       </Suspense>
       <OrbitControls target={[0, 1, 0]} />
       <Stats />
